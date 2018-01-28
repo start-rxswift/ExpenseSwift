b5d1f5ee12e5628ebf3fad26363712fc7cb7ddd5
kimtaesu <kimtaesoo188@gmail.com>
2018-01-28 12:40:45 +0900

    Feature Envy Expense의 type, amount 등을 이용하여 결정을 내리고 있다. 이런 로직은 Expense에 있어야 함.
    Move : isOverage
    to Expense

--- src/main/java/expensereport/Expense.java
+++ src/main/java/expensereport/Expense.java
@@ -1,6 +1,14 @@
 package expensereport;
 
+import static expensereport.Expense.Type.BREAKFAST;
+import static expensereport.Expense.Type.DINNER;
+
 public class Expense {
+    boolean isOverage() {
+        return (type == DINNER && amount > 5000)
+                || (type == BREAKFAST && amount > 1000);
+    }
+
     public enum Type {DINNER, BREAKFAST, CAR_RENTAL}
 
     ;
