2e8f8e85952460fb1c2876b6425c4c8d3b231efe
kimtaesu <kimtaesoo188@gmail.com>
2018-01-28 12:16:33 +0900

    2. SRP 위반 하나의 책임을 가져야 한다. for 루프 안에 비지니스 로직, 메시지 포맷팅 부분 분리

--- src/main/java/expensereport/ExpenseReport.java
+++ src/main/java/expensereport/ExpenseReport.java
@@ -20,6 +20,10 @@ public class ExpenseReport {
             if (expense.type == BREAKFAST || expense.type == DINNER)
                 mealExpenses += expense.amount;
 
+            total += expense.amount;
+        }
+
+        for (Expense expense : expenses) {
             String name = "TILT";
             switch (expense.type) {
                 case DINNER:
@@ -36,10 +40,7 @@ public class ExpenseReport {
                     ((expense.type == DINNER && expense.amount > 5000)
                             || (expense.type == BREAKFAST && expense.amount > 1000)) ? "X" : " ",
                     name, expense.amount / 100.0));
-
-            total += expense.amount;
         }
-
         printTotals(printer, total, mealExpenses);
     }
 
