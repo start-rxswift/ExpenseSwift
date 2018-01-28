fd35f117305ea5f1c7cc4ea66e31985c4623e20e
kimtaesu <kimtaesoo188@gmail.com>
2018-01-28 13:01:31 +0900

    Breakfast, CarRental, Dinner 에 대한 불필요한 Type 을 제거
    Change Signature : type

--- src/main/java/expensereport/DinnerExpense.java
+++ src/main/java/expensereport/DinnerExpense.java
@@ -1,7 +1,7 @@
 package expensereport;
 
 public class DinnerExpense extends Expense {
-    public DinnerExpense(Type dinner, int amount) {
-        super(dinner, amount);
+    public DinnerExpense(int amount) {
+        super(Type.DINNER, amount);
     }
 }
